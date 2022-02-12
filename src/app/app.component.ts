3add586773469b4fb738edad87411e7f6c9ca6e6
hayk manandyan <haykmanandyan@gmail.com>
2022-02-12 12:27:30 +0400

    Dropdown by angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+
+interface DropdownList {
+  name: string;
+}
 
 @Component({
+  host: {
+    '(document:click)': "onClick($event)",
+  },
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-dropdown';
+  public open = false;
+  public cache = 'Select the item';
+  public itemName = this.cache;
+
+  public myDropdownList: DropdownList[] = [
+    {name: 'Clear the option'},
+    {name: 'Monday'},
+    {name: 'Sunday'},
+    {name: 'Tuesday'},
+    {name: 'Wednesday'},
+    {name: 'Thursday'},
+    {name: 'Friday'},
+    {name: 'Saturday'},
+  ];
+
+  @ViewChild('dropdownMenu')
+  dropDown: ElementRef<HTMLDivElement>;
+
+  public toggle(): void {
+    this.open = !this.open;
+  }
+
+  public selectedItem(name: string): void {
+    if (name === this.myDropdownList[0].name) {
+      this.itemName = this.cache;
+    } else {
+      this.itemName = name;
+    }
+    this.open = false;
+  }
+
+  constructor(private elRef: ElementRef) {
+  }
+
+  ngOnInit() {
+  }
+
+  public ngAfterViewInit() {
+    this.cache = this.dropDown.nativeElement.innerText;
+  }
+
+  public onClick(event: Event) {
+    if (event.target === this.dropDown.nativeElement) {
+      this.open = !this.open;
+    } else {
+      this.open = this.elRef.nativeElement.contains(event.target) ? this.open : false;
+    }
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import {FormsModule} from "@angular/forms";
+import {DropdownModule} from "primeng/dropdown";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    DropdownModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
